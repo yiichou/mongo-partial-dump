8a659041089a233affa75e94fbe61d1770a87279
rpechayr <romain.pechayre@applidget.com>
2016-03-05 12:06:58 +0100

    Basic implementation working

--- main.go
+++ main.go
@@ -1,25 +1,88 @@
 package main
 
-import "fmt"
-import "os"
-import "io/ioutil"
-import "gopkg.in/yaml.v2"
+import (
+	"fmt"
+	"gopkg.in/mgo.v2"
+	"gopkg.in/mgo.v2/bson"
+	"gopkg.in/yaml.v2"
+	"io/ioutil"
+	"os"
+	"reflect"
+)
 
 type collectionDescription struct {
 	Collection string
 	Dependency string
 	ForeignKey string `yaml:"foreign_key"`
-	Filters    map[string]string
-	Data       []map[string]string
+	Filters    bson.M
 }
 
-func extractData(todo *collectionDescription) {
-	fmt.Printf("Extracting data from collection %s\n", todo.Collection)
+func extractData(description *collectionDescription, dependentCollection *collectionDescription, sourceDb *mgo.Database, destDb *mgo.Database) {
+
+	sourceCol := sourceDb.C(description.Collection)
+	if dependentCollection != nil {
+		fmt.Printf("Extracting data from collection %s using key %s related to %s\n", description.Collection, description.ForeignKey, dependentCollection.Collection)
+		depCol := destDb.C(dependentCollection.Collection)
+		iterDepCol := depCol.Find(bson.M{}).Iter()
+		batchSize := 50
+		obj := bson.M{}
+		objects := []bson.ObjectId{}
+		for iterDepCol.Next(&obj) {
+			objects = append(objects, obj["_id"].(bson.ObjectId))
+			if len(objects) >= batchSize {
+				//TODO merge with existing filters
+				iter := sourceCol.Find(bson.M{description.ForeignKey: bson.M{"$in": objects}}).Iter()
+				for iter.Next(&obj) {
+					destDb.C(description.Collection).Insert(obj)
+				}
+				objects = []bson.ObjectId{}
+			}
+		}
+
+		//TODO refactor this
+		if len(objects) > 0 {
+			iter := sourceCol.Find(bson.M{description.ForeignKey: bson.M{"$in": objects}}).Iter()
+			for iter.Next(&obj) {
+				destDb.C(description.Collection).Insert(obj)
+			}
+			objects = []bson.ObjectId{}
+		}
+
+	} else {
+		fmt.Printf("%#i \n", description.Filters)
+		if reflect.TypeOf(description.Filters["_id"]) == reflect.TypeOf("") {
+			newId := bson.ObjectIdHex(description.Filters["_id"].(string))
+			description.Filters["_id"] = newId
+		}
+		count, _ := sourceCol.Find(description.Filters).Count()
+		fmt.Printf("%d \n\n\n", count)
+		result := bson.M{}
+		iter := sourceCol.Find(description.Filters).Iter()
+		for iter.Next(&result) {
+			destDb.C(description.Collection).Insert(result)
+		}
+		fmt.Printf("Extracting data from collection %s\n", description.Collection)
+	}
+
 }
 
 func main() {
-	bytes, _ := ioutil.ReadFile(os.Args[1])
-	collections := []*collectionDescription{}
+
+	//TODO: use environment variables
+	sourceCon, _ := mgo.Dial("127.0.0.1:27017")
+	destCon, _ := mgo.Dial("127.0.0.1:27017")
+	defer sourceCon.Close()
+	defer destCon.Close()
+
+	db1 := sourceCon.DB(os.Getenv("SOURCE_DB"))
+	db2 := destCon.DB(os.Getenv("DEST_DB"))
+
+	bytes, err := ioutil.ReadFile(os.Args[1])
+	if err != nil {
+		panic(err)
+	}
+
+	var collections []*collectionDescription
 	doneCollectionCount := 0
 	yaml.Unmarshal([]byte(bytes), &collections)
 	doneCollections := make(map[string]bool)
@@ -36,16 +99,24 @@ func main() {
 			//fmt.Printf("Size of todo %d\n", len(todo))
 			item, todo = todo[0], todo[1:]
 			//fmt.Printf("Size of todo %d\n", len(todo))
-			extractData(item)
+			var dependencyCollection *collectionDescription
+			if len(item.Dependency) > 0 {
+				for _, other := range collections {
+					if other.Collection == item.Dependency {
+						dependencyCollection = other
+						break
+					}
+				}
+			}
+
+			extractData(item, dependencyCollection, db1, db2)
 			doneCollections[item.Collection] = true
 			doneCollectionCount += 1
-			//fmt.Printf("Size of todo %d\n", len(todo))
-			//fmt.Printf("Done collections %d\n", doneCollectionCount)
 			if len(todo) == 0 {
 				break
 			}
 		}
-		//fmt.Printf("--- Done collections %d\n", doneCollectionCount)
+
 		if doneCollectionCount == len(collections) {
 			break
 		}
